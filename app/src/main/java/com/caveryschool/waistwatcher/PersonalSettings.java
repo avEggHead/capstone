6432c6571489eed8f5885de6c9c894183b002aa5
Clinton_Avery <usa.pants45@gmail.com>
2021-01-30 14:52:16 -0600

    Adding personal settings class.

---
+++ app/src/main/java/com/caveryschool/waistwatcher/PersonalSettings.java
@@ -0,0 +1,9 @@
+package com.caveryschool.waistwatcher;
+
+public class PersonalSettings {
+    private float goalWeight;
+    private int goalDate;
+    private char gender;
+    private int heightInFeet;
+    private int heightInInches;
+}
