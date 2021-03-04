c6ec00e0cfcdd7ccda6e06666a6e73dd4bc8a1fa
Clinton_Avery <usa.pants45@gmail.com>
2021-03-04 06:55:34 -0600

    adding more to the date picker

--- app/src/main/java/com/caveryschool/waistwatcher/GoalDatePickerActivity.java
+++ app/src/main/java/com/caveryschool/waistwatcher/GoalDatePickerActivity.java
@@ -33,7 +33,7 @@ public class GoalDatePickerActivity extends AppCompatActivity {
         TextView yearField = this._layout.findViewById(R.id.year_value);
         int yearInt = Integer.parseInt(yearField.getText().toString());
 
-        // subtract 1 from it
+        // add 1 to it
         int newYearInt = yearInt + 1;
 
         // set the new value in the field
@@ -61,4 +61,60 @@ public class GoalDatePickerActivity extends AppCompatActivity {
         Intent settingsScreen = new Intent(this,SettingsActivity.class);
         this.startActivity(settingsScreen);
     }
+
+    public void incrementMonth(View view) {
+        // get the current value of the text field
+        TextView monthField = this._layout.findViewById(R.id.month_value);
+        int monthInt = Integer.parseInt(monthField.getText().toString());
+
+        // add 1 to it if the total is less than 12 otherwise circle around
+        int newMonthInt = 0;
+        if(monthInt < 12){
+            newMonthInt = monthInt + 1;
+        } else {
+            newMonthInt = 1;
+        }
+
+        // set the new value in the field
+        monthField.setText(String.valueOf(newMonthInt));
+    }
+
+    public void decrementMonth(View view) {
+        // get the current value of the text field
+        TextView monthField = this._layout.findViewById(R.id.month_value);
+        int monthInt = Integer.parseInt(monthField.getText().toString());
+
+        // subtract 1 unless it is 1, if it is 1 circle around
+        int newMonthInt = 0;
+        if(monthInt > 1){
+            newMonthInt = monthInt - 1;
+        } else {
+            newMonthInt = 12;
+        }
+
+        // set the new value in the field
+        monthField.setText(String.valueOf(newMonthInt));
+    }
+
+    public void incrementDay(View view) {
+        // get the current value of the text field
+        TextView dayField = this._layout.findViewById(R.id.day_value);
+        int dayInt = Integer.parseInt(dayField.getText().toString());
+
+        // add 1 to it if the total is less than 12 otherwise circle around
+        int newMonthInt = 0;
+        if(dayInt < this.totalDaysInChosenMonth()){
+            newMonthInt = dayInt + 1;
+        } else {
+            newMonthInt = 1;
+        }
+
+        // set the new value in the field
+        dayField.setText(String.valueOf(newMonthInt));
+    }
+
+    private int totalDaysInChosenMonth() {
+        return 30;
+    }
+
 }
